6ae12886ef03e82ff5d09de5a82a0c9acf8f4795
liliandaniel <101359770+liliandaniel@users.noreply.github.com>
2024-04-28 04:46:09 +0200

    asd

---
+++ app/dashboard/categories/columns_categories.tsx
@@ -0,0 +1,58 @@
+"use client";
+import { ColumnDef } from "@tanstack/react-table";
+import { Button } from "@/components/ui/button";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { ArrowUpDown, MoreHorizontal } from "lucide-react";
+import { Badge } from "@/components/ui/badge";
+import { DataTableColumnHeader } from "@/components/ui/data-table/data-table-column-header";
+import { Kategoria } from "@/lib/types";
+import { DeleteCategory, EditCategory } from "@/components/ui/category/buttons";
+
+export const categoryColumns: ColumnDef<Kategoria>[] = [
+  {
+    accessorKey: "KATEGORIAID",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="ID" />
+    ),
+  },
+  {
+    accessorKey: "NEV",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Name" />
+    ),
+  },
+  {
+    accessorKey: "LEIRAS",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Description" />
+    ),
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const category = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem>
+              <EditCategory id={category.KATEGORIAID} />
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <div className="text-red-500">
+                <DeleteCategory id={category.KATEGORIAID} />
+              </div>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];export { categoryColumns as columns };
